64e07947bdf153345caeaa216718e3405bf06189
Nabin12chand <nabin.async.chand@gmail.com>
2023-12-20 18:01:40 +0545

    added: form field were added

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
 import { Container } from "@chakra-ui/react";
+import FormData from "./features/pages/login_page";
 
 function App() {
   return (
-    <Container py={10} color={{ base: "red", sm: "blue", lg: "green" }}>
-      There are many benefits to a joint design and development system. Not only
-      does it bring benefits to the design team, but it also brings benefits to
-      engineering teams. It makes sure that our experiences have a consistent
-      look and feel, not just in our design specs, but in production look and
-      feel, not just in our design specs, but in production
-    </Container>
+    <>
+      <Container py={10} color={{ base: "red", sm: "blue", lg: "green" }}>
+        There are many benefits to a joint design and development system. Not
+        only does it bring benefits to the design team, but it also brings
+        benefits to engineering teams. It makes sure that our experiences have a
+        consistent look and feel, not just in our design specs, but in
+        production look and feel, not just in our design specs, but in
+        production
+      </Container>
+
+      <FormData />
+    </>
   );
 }
 
--- src/features/pages/login_page.tsx
+++ src/features/pages/login_page.tsx
@@ -1,29 +1,67 @@
+// import { useState } from "react";
+
+import {
+  Center,
+  FormControl,
+  FormHelperText,
+  FormLabel,
+  Input,
+} from "@chakra-ui/react";
 import { useState } from "react";
 
- interface myButtonProps{
-    title:string;
-    disabled:boolean
- }
-function Form({title,disabled}: myButtonProps){
-    const [value, setValue]= useState(" ");
-    // const handleChange = useCallback<React.ChangeEventHandler<HTMLInputElement>>((event)=>{
-    //     setValue(event.currentTarget.value);
-    // },[setValue])
-    function handleChange(event:React.ChangeEvent<HTMLInputElement>){
-        setValue(event.currentTarget.value);
-    }
-   
-    return(
-        <>
-    <button disabled={disabled}>
-        
-        {title}</button>
-        <br />
-        <input value={value} onChange={handleChange}/>
-        <p>Value: {value}</p>
-        </>
-     
-    
-    );
+//  interface myButtonProps{
+//     title:string;
+//     disabled:boolean
+//  }
+// function Form({title,disabled}: myButtonProps){
+//     const [value, setValue]= useState(" ");
+//     // const handleChange = useCallback<React.ChangeEventHandler<HTMLInputElement>>((event)=>{
+//     //     setValue(event.currentTarget.value);
+//     // },[setValue])
+//     function handleChange(event:React.ChangeEvent<HTMLInputElement>){
+
+//         setValue(event.currentTarget.value);
+//     }
+
+//     return(
+//         <>
+//     <button disabled={disabled}>
+
+//         {title}</button>
+//         <br />
+//         <input value={value} onChange={handleChange}/>
+//         <p>Value: {value}</p>
+//         </>
+
+//     );
+// }
+// export default Form;
+
+function FormData() {
+  const [input, setInput] = useState("");
+  function handleInputChange(event: React.ChangeEvent<HTMLInputElement>) {
+    setInput(event.currentTarget.value);
+  }
+  const isError = input === "";
+  return (
+    <>
+      <Center px={30} w={600}>
+        <FormControl isInvalid={isError} isRequired>
+          <FormLabel>Email Address</FormLabel>
+          <Input
+            type="email"
+            value={input}
+            onChange={handleInputChange}
+            placeholder="Email address"
+          />
+          {isError ? (
+            <FormHelperText>Enter your email id</FormHelperText>
+          ) : (
+            <FormHelperText>Email is required</FormHelperText>
+          )}
+        </FormControl>
+      </Center>
+    </>
+  );
 }
-export default Form;
+export default FormData;
