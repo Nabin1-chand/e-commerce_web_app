4eebf4393a7dfeb0e32c85a384af1238a465cdc1
Nabin12chand <nabin.async.chand@gmail.com>
2023-12-20 13:25:54 +0545

    setup: reactProject setup with vite and typescript

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import HelloMessage from './features/pages/login_page'
+
+function App() {
+
+  return (
+    <>
+     <h1>Hello react</h1>
+     <HelloMessage title='Press'/>
+    </>
+  );
+}
+
+export default App
---
+++ src/features/pages/login_page.tsx
@@ -0,0 +1,11 @@
+function HelloMessage({title}:{title:string}){
+    return(
+        <>
+              <h2> Hello welcome </h2>
+    <button> title={title}</button>
+        </>
+     
+    
+    );
+}
+export default HelloMessage;
